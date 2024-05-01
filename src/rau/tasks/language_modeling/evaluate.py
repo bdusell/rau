78c27f1d3b57cf5508cce7abb829464ce88ca6e0
Brian DuSell <bdusell@inf.ethz.ch>
2024-05-01 15:37:48 +0200

    Fix evaluation script for language modeling.

--- src/rau/tasks/language_modeling/evaluate.py
+++ src/rau/tasks/language_modeling/evaluate.py
@@ -7,7 +7,7 @@ import sys
 from rau.tasks.common.data import load_prepared_data_file
 from rau.tasks.language_modeling.data import load_vocabularies
 from rau.tasks.language_modeling.model import LanguageModelingModelInterface
-from rau.tasks.language_modeling.training_loop import generate_batches, evaluate
+from rau.tasks.language_modeling.training_loop import LanguageModelingTrainingLoop
 
 def main():
 
@@ -52,11 +52,28 @@ def main():
         parser.error('either --training-data and --input or --input-file is required')
 
     device = model_interface.get_device(args)
-    sources = load_prepared_data_file(input_file)
+    examples = load_prepared_data_file(input_file)
     vocabs = load_vocabularies(args, parser)
     saver = model_interface.construct_saver(args)
-    batches = generate_batches(sources, args.batching_max_tokens)
-    result = evaluate(saver.model, batches, vocabs, model_interface, device)
+    # Create a dummy training loop object so we can reuse the batching and
+    # evaluation methods.
+    # TODO Refactor this.
+    training_loop = LanguageModelingTrainingLoop(
+        show_progress=False,
+        max_epochs=0,
+        random_shuffling_seed=0,
+        max_tokens_per_batch=args.batching_max_tokens,
+        optimizer='SGD',
+        initial_learning_rate=1.0,
+        label_smoothing_factor=None,
+        gradient_clipping_threshold=None,
+        early_stopping_patience=1,
+        learning_rate_patience=1,
+        learning_rate_decay_factor=0.5,
+        examples_per_checkpoint=1
+    )
+    batches = training_loop.generate_batches(examples, args.batching_max_tokens)
+    result = training_loop.evaluate(saver.model, model_interface, vocabs, batches)
     result['perplexity'] = math.exp(result['cross_entropy_per_token'])
     json.dump(result, sys.stdout, indent=2)
     print(file=sys.stdout)
