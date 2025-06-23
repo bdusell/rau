01f85c7aac11abf1a2d4a1cd86cc513cb1272dfd
Brian DuSell <brian.dusell@inf.ethz.ch>
2025-06-23 12:02:03 -0400

    Fix redirect.

--- scripts/build_docs_redirect.bash
+++ scripts/build_docs_redirect.bash
@@ -1,5 +1,4 @@
 set -euo pipefail
 sed "
-    s|{{BASE_URL}}|https://bdusell.github.io/rau|g;
-    s|{{NEW_PATH}}|/main/index.html|g
+    s|{{NEW_URL}}|https://bdusell.github.io/rau/main/index.html|g
 " docs/_redirect.html > docs/dist/index.html
