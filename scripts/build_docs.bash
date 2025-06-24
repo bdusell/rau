0bb6fea3f811dd30516475d3e10281c16d93a19e
Brian DuSell <brian.dusell@inf.ethz.ch>
2025-06-24 01:59:54 -0400

    Configure latest release version in docs.

--- scripts/build_docs.bash
+++ scripts/build_docs.bash
@@ -1,3 +1,7 @@
 set -euo pipefail
-sphinx-multiversion docs docs/dist
-bash scripts/build_docs_redirect.bash
+latest_version=$( \
+    sphinx-multiversion --dump-metadata docs docs/dist | \
+    python scripts/get_latest_version.py \
+)
+sphinx-multiversion -D smv_latest_version="$latest_version" docs docs/dist
+bash scripts/build_docs_redirect.bash "$latest_version"
--- scripts/build_docs_redirect.bash
+++ scripts/build_docs_redirect.bash
@@ -1,4 +1,5 @@
 set -euo pipefail
+latest_version=$1
 sed "
-    s|{{NEW_URL}}|https://bdusell.github.io/rau/main/index.html|g
+    s|{{NEW_URL}}|https://bdusell.github.io/rau/$latest_version/index.html|g
 " docs/_redirect.html > docs/dist/index.html
