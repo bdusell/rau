aabcca19737ffbc6ddc7f54cb337abec9d7bea5b
Brian DuSell <brian.dusell@inf.ethz.ch>
2025-06-24 02:06:53 -0400

    Handle redirect when there is no release version.

--- scripts/build_docs_redirect.bash
+++ scripts/build_docs_redirect.bash
@@ -1,5 +1,5 @@
 set -euo pipefail
-latest_version=$1
+latest_version=${1:-main}
 sed "
     s|{{NEW_URL}}|https://bdusell.github.io/rau/$latest_version/index.html|g
 " docs/_redirect.html > docs/dist/index.html
