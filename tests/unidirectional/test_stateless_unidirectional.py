0a0be65941300f2e9153542bd6632aec17b7a531
Brian DuSell <brian.dusell@inf.ethz.ch>
2025-07-22 20:48:47 +0200

    Add (failing) unit tests for lazy outputs.

--- tests/unidirectional/test_stateless_unidirectional.py
+++ tests/unidirectional/test_stateless_unidirectional.py
@@ -1,6 +1,10 @@
 import torch
 
-from rau.unidirectional import StatelessUnidirectional, StatelessLayerUnidirectional
+from rau.unidirectional import (
+    Unidirectional,
+    StatelessUnidirectional,
+    StatelessLayerUnidirectional
+)
 
 class MyStateless(StatelessUnidirectional):
 
@@ -54,3 +58,62 @@ def test_custom_args():
         output = state.output()
         assert output.size() == (batch_size, input_size)
         torch.testing.assert_close(output, forward_output[:, i])
+
+class CountingStateless(StatelessUnidirectional):
+
+    def __init__(self):
+        super().__init__()
+        self.num_forward_single_calls = 0
+
+    def forward_single(self, input_tensor):
+        self.num_forward_single_calls += 1
+        return input_tensor
+
+class CountingStateful(Unidirectional):
+
+    def __init__(self):
+        super().__init__()
+        self.num_next_calls = 0
+        self.num_output_calls = 0
+
+    def initial_state(self, batch_size):
+        return self.State(self, None)
+
+    class State(Unidirectional.State):
+
+        def __init__(self, parent, input_tensor):
+            super().__init__()
+            self.parent = parent
+            self.input_tensor = input_tensor
+
+        def next(self, input_tensor):
+            self.parent.num_next_calls += 1
+            return self.parent.State(self.parent, input_tensor)
+
+        def output(self):
+            assert self.input_tensor is not None
+            self.parent.num_output_calls += 1
+            return self.input_tensor
+
+def test_lazy_outputs_with_next():
+    A = CountingStateless()
+    B = CountingStateless()
+    C = CountingStateful()
+    D = CountingStateless()
+    E = CountingStateless()
+    M = A | B | C | D | E
+    batch_size = 3
+    sequence_length = 13
+    model_size = 5
+    generator = torch.manual_seed(123)
+    input_sequence = torch.rand((batch_size, sequence_length, model_size), generator=generator)
+    state = M.initial_state(batch_size)
+    for input_tensor in input_sequence.transpose(0, 1):
+        state = state.next(input_tensor)
+    state.output()
+    assert A.num_forward_single_calls == sequence_length
+    assert B.num_forward_single_calls == sequence_length
+    assert C.num_next_calls == sequence_length
+    assert C.num_output_calls == 1
+    assert D.num_forward_single_calls == 1
+    assert E.num_forward_single_calls == 1
