e77a532a7ca21e74426cb98bad01ecdaff2092c1
Brian DuSell <brian.dusell@inf.ethz.ch>
2025-08-04 14:13:10 +0200

    Add some docs for vocab classes.

--- src/rau/vocab/__init__.py
+++ src/rau/vocab/__init__.py
@@ -1,3 +1,5 @@
+r"""This module provides tools for mapping token types to integer IDs."""
+
 from .base import Vocabulary, VocabularyBuilder
 from .to_int import build_to_int_vocabulary, ToIntVocabulary, ToIntVocabularyBuilder
 from .to_string import build_to_string_vocabulary, ToStringVocabulary, ToStringVocabularyBuilder
