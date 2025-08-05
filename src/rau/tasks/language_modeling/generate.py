11fb4fcf1b410c2b41e9e5d0de81a79dac56f973
Brian DuSell <brian.dusell@inf.ethz.ch>
2025-08-06 00:54:39 +0200

    Add option to generate outputs for multiple prompt files.

--- src/rau/tasks/language_modeling/generate.py
+++ src/rau/tasks/language_modeling/generate.py
@@ -1,8 +1,10 @@
 import pathlib
+import sys
 
 import torch
 
 from rau.tasks.common.command import Command
+from rau.tasks.common.data import load_prepared_data_file
 from rau.tasks.language_modeling.model import LanguageModelingModelInterface
 from rau.tasks.language_modeling.vocabulary import load_vocabulary_data_from_file
 from rau.generation.sample import sample_single
@@ -30,6 +32,17 @@ class LanguageModelingGenerateCommand(Command):
         parser.add_argument('--vocabulary-file', type=pathlib.Path,
             help='A .vocab file to be used as the vocabulary. This overrides '
                  '--training-data.')
+        parser.add_argument('--prompt-datasets', nargs='+',
+            help='Optional names of datasets in the training data directory '
+                 'that will be used to prompt the language model. The file '
+                 '<training-data>/datasets/<prompt-dataset>/main.prepared will '
+                 'be used as input. Outputs will be generated for each example '
+                 'in each dataset. The name "training" can be used to evaluate '
+                 'on the training data.')
+        parser.add_argument('--output', type=pathlib.Path,
+            help='Optional directory where output files will be written when '
+                 'using --prompt-datasets. A separate file will be written for '
+                 'each prompt dataset.')
         parser.add_argument('--mode',
             choices=['random', 'greedy', 'beam-search'],
             default='random',
@@ -70,43 +83,70 @@ class LanguageModelingGenerateCommand(Command):
             load_vocabulary_data_from_file(vocab_file),
             include_embedding_vocab=False
         )
-        model.eval()
-        with torch.inference_mode():
-            initial_state = model_interface.get_initial_state(
-                saver.model,
-                batch_size=1,
-                device=device
-            )
-            def generate_outputs():
-                match args.mode:
-                    case 'random':
-                        if args.random_seed is not None:
-                            generator = torch.Generator(device=device).manual_seed(args.random_seed)
-                        else:
-                            generator = None
-                        for _ in range(args.num_samples):
-                            yield sample_single(
-                                initial_state=initial_state,
-                                eos_symbol=eos_index,
-                                max_length=args.max_length,
-                                generator=generator
-                            )
-                    case 'greedy':
-                        yield decode_greedily_single(
+
+        if args.output is not None:
+            args.output.mkdir(parents=True, exist_ok=True)
+
+        def generate_outputs(initial_state):
+            match args.mode:
+                case 'random':
+                    if args.random_seed is not None:
+                        generator = torch.Generator(device=device).manual_seed(args.random_seed)
+                    else:
+                        generator = None
+                    for _ in range(args.num_samples):
+                        yield sample_single(
                             initial_state=initial_state,
-                            eos_symbol=eos_index,
-                            max_length=args.max_length
-                        )
-                    case 'beam-search':
-                        yield beam_search_single(
-                            initial_state=initial_state,
-                            beam_size=args.beam_size,
                             eos_symbol=eos_index,
                             max_length=args.max_length,
-                            device=device
+                            generator=generator
                         )
-            for s in generate_outputs():
-                print(' '.join(map(vocab.to_string, s)))
+                case 'greedy':
+                    yield decode_greedily_single(
+                        initial_state=initial_state,
+                        eos_symbol=eos_index,
+                        max_length=args.max_length
+                    )
+                case 'beam-search':
+                    yield beam_search_single(
+                        initial_state=initial_state,
+                        beam_size=args.beam_size,
+                        eos_symbol=eos_index,
+                        max_length=args.max_length,
+                        device=device
+                    )
+
+        model.eval()
+        with torch.inference_mode():
+            if args.prompt_datasets is not None:
+                for prompt_dataset in args.prompt_datasets:
+                    input_file_name = args.training_data / 'datasets' / prompt_dataset / 'main.prepared'
+                    prompts = load_prepared_data_file(input_file_name)
+                    if args.output is not None:
+                        output_file_name = args.output / f'{prompt_dataset}.tok'
+                        print(f'writing {output_file_name}')
+                        output_file = output_file_name.open('w')
+                    else:
+                        output_file = sys.stdout
+                    with output_file:
+                        for prompt in prompts:
+                            prompt = prompt.to(device)
+                            state = model_interface.get_initial_state(
+                                saver.model,
+                                batch_size=1,
+                                device=device
+                            )
+                            state = state.fastforward(prompt.unsqueeze(0))
+                            for s in generate_outputs(state):
+                                print(' '.join(map(vocab.to_string, s)), file=output_file)
+            else:
+                initial_state = model_interface.get_initial_state(
+                    saver.model,
+                    batch_size=1,
+                    device=device
+                )
+                for s in generate_outputs(initial_state):
+                    print(' '.join(map(vocab.to_string, s)))
 
 if __name__ == '__main__':
     LanguageModelingGenerateCommand().main()
