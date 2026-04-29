c20601b38e04d024df715df365d48fdfe5d5d6c1
Brian DuSell <brian.dusell@inf.ethz.ch>
2026-04-29 22:46:55 +0200

    Update PE consistency test.

--- tests/test_sinusoidal_positional_encodings.py
+++ tests/test_sinusoidal_positional_encodings.py
@@ -12,12 +12,11 @@ def test_dmodel_sizes(d_model) -> None:
 
 def test_consistency() -> None:
     device = torch.device('cpu')
-    a_length = 4
-    a_d_model = 4
-    a = sinusoidal_positional_encodings(a_length, a_d_model, device)
-    print(a)
-    b_length = 10
-    b_d_model = 4
-    b = sinusoidal_positional_encodings(b_length, b_d_model, device)
-    print(b)
-    torch.testing.assert_close(b[:a_length,:a_d_model], a)
+    d_model = 128
+    a_length = 50
+    a = sinusoidal_positional_encodings(a_length, d_model, device)
+    assert a.size() == (a_length, d_model)
+    b_length = 100
+    b = sinusoidal_positional_encodings(b_length, d_model, device)
+    assert b.size() == (b_length, d_model)
+    torch.testing.assert_close(b[:a_length], a)
